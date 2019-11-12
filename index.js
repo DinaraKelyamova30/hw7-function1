3e08888933995fa53b5dedf086799c1eed9448fc
DinaraKelyamova30 <kelyamova30@gmail.com>
2019-11-12 14:48:56 +0200

    js

---
+++ index.js
@@ -0,0 +1,13 @@
+debugger;
+function f(...arr) {
+    let n = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > n) {
+            n = arr[i];
+        }
+    }
+    return n;
+}
+
+console.log(f(5, -2, 10));
+console.log(f(5, -2, 30, 6));
